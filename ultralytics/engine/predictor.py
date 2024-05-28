53ee8d0034cab66edd3d2c44ffe2478099d7a1ed
aaavvvrrr <35411851+aaavvvrrr@users.noreply.github.com>
2024-05-28 20:52:33 +0300

    Update predictor.py

--- ultralytics/engine/predictor.py
+++ ultralytics/engine/predictor.py
@@ -225,9 +225,12 @@ class BasePredictor:
 
             # Warmup model
             if not self.done_warmup:
-                self.model.warmup(imgsz=(1 if self.model.pt or self.model.triton else self.dataset.bs, 3, *self.imgsz))
+                try:
+                    nch=source.shape[2]
+                except: 
+                    nch=3
+                self.model.warmup(imgsz=(1 if self.model.pt or self.model.triton else self.dataset.bs, nch, *self.imgsz))
                 self.done_warmup = True
-
             self.seen, self.windows, self.batch = 0, [], None
             profilers = (
                 ops.Profile(device=self.device),
