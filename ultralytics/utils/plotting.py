c447a0a3c57ecab435f694422005dd00fb54c935
aaavvvrrr <35411851+aaavvvrrr@users.noreply.github.com>
2024-09-14 02:00:11 +0300

    Update plotting.py

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -984,7 +984,8 @@ def plot_images(
     mosaic = np.full((int(ns * h), int(ns * w), 3), 255, dtype=np.uint8)  # init
     for i in range(bs):
         x, y = int(w * (i // ns)), int(h * (i % ns))  # block origin
-        mosaic[y : y + h, x : x + w, :] = images[i].transpose(1, 2, 0)
+        # mosaic[y : y + h, x : x + w, :] = images[i].transpose(1, 2, 0)
+        mosaic  [y : y + h, x : x + w, : images[i].shape[0]] = images[i].transpose(1, 2, 0)  # avr
 
     # Resize (optional)
     scale = max_size / ns / max(h, w)
